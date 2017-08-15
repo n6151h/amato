afc8106af7d5a2e2dbbe3c7dad515da3c7c1fe5e
Nick Seidenman <seidenman@wehi.edu.au>
2017-08-15 22:17:34 +1000

    Added schedule API.

--- people/views.py
+++ people/views.py
@@ -1,3 +1,37 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import *
+
+from .serializers import *
+
+class SeasonViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows "seasons" to be viewed or edited.
+    """
+    queryset = Season.objects.all()
+    serializer_class = SeasonSerializer
+
+class ProductionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows "productions" to be viewed or edited.
+    """
+    queryset = Production.objects.all()
+    serializer_class = ProductionSerializer
+
+class ShowViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows "shows" to be viewed or edited.
+    """
+    queryset = Show.objects.all()
+    serializer_class = ShowSerializer
+
+class CallViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows "calls" to be viewed or edited.
+    """
+    queryset = Call.objects.all()
+    serializer_class = CallSerializer
+
